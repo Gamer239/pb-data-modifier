d366069c9f979c30f41e3af098e611f764e9d9eb
Zach Malinowski <gamer23170@gmail.com>
2022-03-04 11:07:55 -0600

    Update/Add Comments
    This commit updates and adds comments to make the code easier to read.

--- fix-data.py
+++ fix-data.py
@@ -1,5 +1,6 @@
 import sys, argparse, csv, os
 
+#Perform an operation(s) on a row.
 def fill_row(row, lastRow):
     if row['Practitioner'] == "":
         row['Practitioner'] = lastRow['Practitioner']
@@ -8,6 +9,7 @@ def fill_row(row, lastRow):
     return(row)
 
 def do_work(filename, outputfilename):
+    #Check to make sure that the input file exists and exit on error
     if os.path.isfile(filename) == False:
         print("Error: Input file not found")
         exit()
@@ -15,24 +17,30 @@ def do_work(filename, outputfilename):
     #Open the input file for reading
     with open(filename , newline='', encoding='utf8') as csvfile:
         csvcontents = csv.DictReader(csvfile)
+
+        #Initialize lastrow contents
         lastRow = ""
 
-        #open the output file
+        #open the output file for writing the modified results
         with open(outputfilename, 'w', newline='', encoding='utf8') as outputfile:
             writer = csv.DictWriter(outputfile, fieldnames=csvcontents.fieldnames)
+
+            #Write the CSV Header Information
             writer.writeheader()
+
+            #Iterate through each data row in the table
             for row in csvcontents:
-                #fill in the row's contents
+                #save the current row with any data modified from fill_row
                 row = fill_row(row, lastRow)
 
-                #save the last row
+                #save the current row to use for the next row's calculations
                 lastRow = row
 
                 #write the modified row to the output file
                 writer.writerow(row)
 
 if __name__ == '__main__':
-    # Create the parser and add arguments
+    # Create the argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', type=str, nargs='?', help="Name of the input file", required=True)
     parser.add_argument('-o', type=str, nargs='?', help="Name of the file for the output", required=False)
