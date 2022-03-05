2ce71020975cb6fba1d15f9a1d158b95ac907071
Zach Malinowski <gamer23170@gmail.com>
2022-03-04 23:03:14 -0600

    Add Sum Support
    This commit adds support for summing columns of data.

--- fix-data.py
+++ fix-data.py
@@ -15,7 +15,31 @@ def fill_row(row, lastRow):
         row['AmountPaid'] = 0.00
     return(row)
 
-def do_work(filename, outputfilename):
+#Add the current row data to the sum totals and return the updated values
+def sum_row(row, sum_fields, current_sums):
+    #Save a copy of the row to manipulate
+    sums = row
+
+    #Loop through each of the field names
+    for field in sum_fields:
+
+        #Check of the field name exists in the row
+        if field in current_sums.keys():
+
+            #Check if the field contents are only numeric
+            if type(sums[field]) == float or sums[field].replace('.','',1).isdigit():
+                sums[field] = float(sums[field]) + float(current_sums[field])
+            else:
+                sums[field] = sums[field] + current_sums[field]
+                if len(sums[field]) > 1000:
+                    print("Error: You're trying to perform a sum operation on a string. Check your -s arguments.")
+                    exit()
+            
+    #return the new sum dictionary row
+    return(sums)
+
+
+def do_work(filename, outputfilename, sum_args):
     #Check to make sure that the input file exists and exit on error
     if os.path.isfile(filename) == False:
         print("Error: Input file not found")
@@ -27,6 +51,9 @@ def do_work(filename, outputfilename):
 
         #Initialize lastrow contents
         lastRow = ""
+
+        #nitialize sum contents
+        sum_data = {}
 
         #open the output file for writing the modified results
         with open(outputfilename, 'w', newline='', encoding='utf8') as outputfile:
@@ -43,18 +70,32 @@ def do_work(filename, outputfilename):
                 #save the current row to use for the next row's calculations
                 lastRow = row
 
+                #Add the sum data if it was defined in the arguments
+                if sum_args != None:
+                    sum_data = sum_row(row, sum_args, sum_data)
+
                 #write the modified row to the output file
                 writer.writerow(row)
+
+        #Check to see if the sum argument was added
+        if sum_args != None:
+            #Loop through each sum argument
+            for field in sum_args:
+                #Check of the provided argument has a key in the computed dictionary
+                if field in sum_data.keys():
+                    #print the computed result
+                    print("For field: " + field + " the total is " + str(sum_data[field]))
 
 if __name__ == '__main__':
     # Create the argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', type=str, nargs='?', help="Name of the input file", required=True)
     parser.add_argument('-o', type=str, nargs='?', help="Name of the file for the output (Default=output.csv)", required=False, default="output.csv")
+    parser.add_argument('-s', nargs='+', help="A space separated list of fields names to sum", required=False)
 
     # Parse and print the results
     args = parser.parse_args()
     if args.i == None:
         parser.print_help()
     else:
-        do_work(args.i, args.o)
+        do_work(args.i, args.o, args.s)
