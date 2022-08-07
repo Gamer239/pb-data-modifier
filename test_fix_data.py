a6e67c5a445c917d2fcea4a8278f322995361140
Zach Malinowski <gamer23170@gmail.com>
2022-08-07 08:02:56 -0700

    Setup base for testing LineItemProfit

--- test_fix_data.py
+++ test_fix_data.py
@@ -3,5 +3,9 @@ __copyright__ = "Copyright (C) 2022 Gamer239"
 __license__ = "General Public Licence v2"
 __version__ = "1.0"
 
-import pytest
+import pytest, importlib
+fix_data = importlib.import_module("fix-data")
 
+def test_lineitemprofit():
+    
+    assert True == True
